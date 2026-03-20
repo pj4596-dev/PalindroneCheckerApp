b6a4f074c20cc06a2e086b4ad1f1320d396a3c36
vishnu2006965 <vr9259@srmist.edu.in>
2026-03-20 10:56:30 +0530

    uc-2

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,4 +1,28 @@
 package PACKAGE_NAME;
 
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp
+{
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String input = "madam";
+
+        // Assume it is a palindrome initially
+        boolean isPalindrome = true;
+
+        // Compare characters from both ends
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+        }
+    }
 }
